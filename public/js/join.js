8303f40f27370555cbb1c3afa6304dd3b1135041
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2020-10-30 19:03:44 +0530

    Add join and create radio button

---
+++ public/js/join.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#radioBut2").click(function () {
+    $("#selectModelFormDiv").css({ display: "block" });
+  });
+
+  $("#radioBut1").click(function () {
+    $("#selectModelFormDiv").css({ display: "none" });
+  });
+});
